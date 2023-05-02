cf941dd31b8e11a429a44f586d63143ed0159dfa
Serhii Kupriienko <skupriienko@anaconda.com>
2023-05-02 13:43:47 +0300

    Release 1.13.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,6 @@
-  
+:: cmd
+echo "Building %PKG_NAME%."
+
 set "ONNX_ML=1"
 set CONDA_PREFIX=%LIBRARY_PREFIX%
 set CMAKE_GENERATOR="Visual Studio 15 2017"
@@ -7,4 +9,4 @@ set CMAKE_ARGS="-DONNX_USE_PROTOBUF_SHARED_LIBS=ON -DProtobuf_USE_STATIC_LIBS=OF
 set "PYTHON_EXECUTABLE=%PYTHON%"
 set "PYTHON_LIBRARIES=%LIBRARY_LIB%"
 set USE_MSVC_STATIC_RUNTIME=0
-%PYTHON% -m pip install --no-deps --no-use-pep517 --ignore-installed --verbose .
+%PYTHON% -m pip install --no-deps --no-use-pep517 --ignore-installed --no-build-isolation --verbose .
